d568485d9cd9513115884c8bef595182bf92bc61
andrew21-mch <nfonandrew73@gmail.com>
2022-04-29 09:28:08 +0100

    created custom exception handler class to handle exceptions

---
+++ src/main/java/com/nasiatech/internmanager/exceptions/InternNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nasiatech.internmanager.exceptions;
+
+public class InternNotFoundException extends Throwable {
+    public InternNotFoundException(String message) {
+        super(message);
+    }
+}
