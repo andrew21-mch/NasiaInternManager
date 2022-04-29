3d63686eec4e508ecee5807d4d824487e2e3240a
andrew21-mch <nfonandrew73@gmail.com>
2022-04-29 09:25:00 +0100

    produced the interface for jpa repo

---
+++ src/main/java/com/nasiatech/internmanager/intern/InternRepository.java
@@ -0,0 +1,6 @@
+package com.nasiatech.internmanager.intern;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InternRepository extends JpaRepository<Intern, Long> {
+}
